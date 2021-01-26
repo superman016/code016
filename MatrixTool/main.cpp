a6def3f1b95819f9fc551eb7b59404505b142c5b
superman016 <superman016@qq.com>
2021-01-26 17:45:50 +0800

    update

--- MatrixTool/main.cpp
+++ MatrixTool/main.cpp
@@ -7,11 +7,11 @@ int main() {
 	double a[3] = {1, 2, 3};
 	double b[3][3] = {{3, 2, 1}, {3, 2, 1}, {3, 2, 1}};
 	
-	Matrix A(3);                  //定义一个长度为 1 的一维矩阵
-	A.setByArray(a);              //调用类的 setByArray 将一维数组数组转化为矩阵 
-	 
+	Matrix A(3);    //定义一个长度为 1 的一维矩阵
+	A.setByArray(a);//调用类的 setByArray 将一维数组数组转化为矩阵 
+
 	Matrix B = to_matrix(b, 3, 3);//使用全局函数 to_matrix 将一个二维数组转化为一个二维矩阵 
-	
+
 	Matrix res = A * B;           //计算矩阵 A 和 B 的乘积并用矩阵对象 res 接收结果 
 	
 	A.print();       //打印矩阵 A 的值 
